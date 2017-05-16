1952fe8dca5bb14d402c7d153ec7d255c3f82675
PoffDev <joshuapoffenberger@gmail.com>
2017-05-15 21:46:55 -0400

    webpack sorta running

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+
+  // will allow errors in console to display on correct page and line
+  devtool: 'source-map',
+
+  // This code will be compiled
+  entry: "./app/app.js",
+
+  // Then output into this file
+  output: {
+    filename: "app/public/bundle.js",
+    publicPath: './app/public'
+  },
+
+  module: {
+      loaders: [
+        {
+          test: /.jsx?$/,
+
+          loader: 'babel-loader',
+          exclude: /node_modules/,
+          query: {
+            presets: ['react', 'es2015']
+          }
+        }
+      ]
+    },
+
+// // This lets us debug our react code in chrome dev tools. Errors will have lines and file names
+// // Without this the console says all errors are coming from just coming from bundle.js
+// devtool: "eval-source-map"
+};
