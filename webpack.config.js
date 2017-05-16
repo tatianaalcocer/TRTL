3cb1a970c0d7bf4768df8071df65bfbd79c86893
PoffDev <joshuapoffenberger@gmail.com>
2017-05-15 21:56:00 -0400

    updated app folder structure, added materalize, and imported into html file

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
       loaders: [
         {
           test: /.jsx?$/,
-
+          exclude: /(node_modules|bower_components)/,
           loader: 'babel-loader',
           exclude: /node_modules/,
           query: {
