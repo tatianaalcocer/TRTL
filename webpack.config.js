4fa9cfe9a1c5bb417830a943d0d69942dfbb8307
PoffDev <joshuapoffenberger@gmail.com>
2017-05-17 20:17:19 -0400

    updated webpack, still not working...

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+var path = require('path');
+
 module.exports = {
 
   // will allow errors in console to display on correct page and line
@@ -9,22 +11,40 @@ module.exports = {
   // Then output into this file
   output: {
     filename: "app/public/bundle.js",
-    publicPath: './app/public'
+    publicPath: './app/Public'
   },
 
   module: {
-      loaders: [
-        {
-          test: /.jsx?$/,
-          exclude: /(node_modules|bower_components)/,
-          loader: 'babel-loader',
-          exclude: /node_modules/,
-          query: {
-            presets: ['react', 'es2015']
-          }
+    rules: [
+        { test: /\.js$/,
+            loader: 'babel-loader',
+            query: {
+                presets: [
+                    'es2015',
+                    'react'
+                ],
+            plugins: []
+        },
+
+        include: [
+            path.resolve(__dirname, 'app')
+        ]
+        },  {
+            test: /\.json$/,
+            loader: "json-loader"
         }
-      ]
-    },
+    ]
+  },
+
+  resolve: {
+      modules: [
+          path.join(process.cwd(), 'app'),
+          'node_modules'
+      ],
+      extensions: ['.js', '.json']
+  },
+
+  devtool: false
 
 // // This lets us debug our react code in chrome dev tools. Errors will have lines and file names
 // // Without this the console says all errors are coming from just coming from bundle.js
